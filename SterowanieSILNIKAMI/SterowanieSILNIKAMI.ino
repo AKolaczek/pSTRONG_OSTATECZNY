e884c77d6feda2a1c7173d9e61f5f84a9613173d
Wojciech Antoszczyszyn <261300@student.pwr.edu.pl>
2022-02-21 12:53:33 +0100

    .

---
+++ SterowanieSILNIKAMI/SterowanieSILNIKAMI.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SterowanieSILNIKAMI.ino
+ Created:	2/21/2022 12:53:11 PM
+ Author:	hp
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
